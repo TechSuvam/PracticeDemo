200b2ec9689fbe140f0d7dd024c907915532ca93
Suvam Chanda <Suvam_Chanda@epam.com>
2022-07-31 09:19:43 +0530

    Add project files.

---
+++ PracticeDemo/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace PracticeDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            var singleton = Singleton.SingletonProp.GetType();
+            var staticType= StaticClass.GetType();
+
+        }
+
+
+    }
+
+    public sealed class Singleton
+    {
+        private static Singleton _singletonInstance = null;
+        private Singleton()
+        {
+
+        }
+
+        public static Singleton SingletonProp
+        {
+            get
+            {
+                if(_singletonInstance == null)
+                {
+                    _singletonInstance = new Singleton();
+                }
+                return _singletonInstance;
+            }
+        }
+
+        public string GetType()
+        {
+            return "Singleton Class";
+        }
+    }
+
+    public static class StaticClass
+    {
+        public static string GetType()
+        {
+            return "Static Class";
+        }
+    }
+
+    
+}
